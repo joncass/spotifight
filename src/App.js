eba51792940ea44f420a5bccac2e77027d55f66b
Jon Cass <joncass@gmail.com>
2017-09-20 20:03:36 -0400

    makes rounds and matchups and a winner

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { fetchTrackData } from './data'
 import TrackMatchup from './components/TrackMatchup'
 
 const _randomElement = arr => (arr[Math.floor(Math.random()*arr.length)])
+const POOL_SIZE = 16
 
 class App extends Component {
   constructor(props) {
@@ -18,35 +19,61 @@ class App extends Component {
     this.state = {
       tracks: null,
     }
+
+    this.round = 1
     this.matchup = 0
-    this.prefs = []
+
+    this.currentRoundTracks = []
+    this.nextRoundTracks = []
+
     this._fetchData()
   }
 
   _fetchData() {
-    this.setState({ tracks: null })
-    this.matchup = this.matchup + 1
-
     fetchTrackData().then(trackData => {
-      const randomCategory1 = _randomElement(trackData)
-      const randomTrack1 = _randomElement(randomCategory1.tracks)
-      const randomCategory2 = _randomElement(trackData)
-      const randomTrack2 = _randomElement(randomCategory2.tracks)
-      this.setState({ tracks: [
-        {
-          url: randomTrack1.url,
-        },
-        {
-          url: randomTrack2.url,
-        },
-      ]})
+      while (this.currentRoundTracks.length < POOL_SIZE) {
+        const randomCategory = _randomElement(trackData)
+        const randomTrack = _randomElement(randomCategory.tracks)
+        this.currentRoundTracks.push({ url: randomTrack.url })
+      }
+
+      this._createMatchup()
     })
   }
 
+  _createMatchup() {
+    this.matchup = this.matchup + 1
+    this.setState({
+      tracks: [
+        this.currentRoundTracks.pop(), this.currentRoundTracks.pop()
+      ]
+    })
+  }
+
+  _newRound() {
+    this.currentRoundTracks = this.nextRoundTracks.slice()
+    this.nextRoundTracks = []
+    this._createMatchup()
+
+    this.round = this.round + 1
+    this.matchup = 1
+  }
+
   choose(track) {
-    this.prefs.push(track)
-    console.log(this.prefs)
-    this._fetchData()
+    this.nextRoundTracks.push(track)
+    this.setState({ tracks: null }, () => {
+
+      if (this.currentRoundTracks.length) {
+        this._createMatchup()
+      }
+      else if (this.nextRoundTracks.length > 1) {
+        this._newRound()
+      }
+      else {
+        const winner = this.nextRoundTracks[0]
+        this.setState({ winner })
+      }
+    })
   }
 
   render() {
@@ -58,21 +85,30 @@ class App extends Component {
         columns={2}
         padded
       >
-        <Grid.Column>
-          <Header>Matchup #{this.matchup}</Header>
-          {
-            this.state.tracks
-            ?
-            <TrackMatchup
-              tracks={this.state.tracks}
-              onChoose={track => { this.choose(track) }}
-            />
-            :
-            <Segment basic>
-              <Loader active size='big' />
-            </Segment>
-          }
-        </Grid.Column>
+        {
+          this.state.winner
+          ?
+          <div>
+            We have a <a href={this.state.winner.url}>winner!</a>
+          </div>
+          :
+          <Grid.Column>
+            <Header as='h1'>Round {this.round}</Header>
+            <Header as='h3'>Matchup {this.matchup}</Header>
+            {
+              this.state.tracks
+              ?
+              <TrackMatchup
+                tracks={this.state.tracks}
+                onChoose={track => { this.choose(track) }}
+              />
+              :
+              <Segment basic>
+                <Loader active size='big' />
+              </Segment>
+            }
+          </Grid.Column>
+        }
       </Grid>
     )
   }
